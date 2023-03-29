8da16160b4a87e2b074f2348c848e51fec8ff968
Akimori <wee.bscl@gmail.com>
2023-03-30 01:17:45 +0800

    added view2 route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './components/search.component';
 import { CharacterListComponent } from './components/character-list.component';
+import { CharacterDetailsComponent } from './components/character-details.component';
 
 const routes: Routes = [
   { path: '', component: SearchComponent },
   { path: 'characters', component: CharacterListComponent },
+  { path: 'characters/:id', component: CharacterDetailsComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
