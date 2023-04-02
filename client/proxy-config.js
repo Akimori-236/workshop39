578a1436a77b8c3dbf7f878980f8542434d18beb
Akimori <wee.bscl@gmail.com>
2023-04-02 19:02:12 +0800

    proxy-config fix

--- client/proxy-config.js
+++ client/proxy-config.js
@@ -1,4 +1,4 @@
-module.exports = [
+const PROXY_CONFIG = [
   {
     context: ["/**"],
     target: "http://localhost:8080",
@@ -6,3 +6,4 @@ module.exports = [
     logLevel: "debug",
   },
 ];
+module.exports = PROXY_CONFIG;
